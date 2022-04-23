0638332464c5fa6970802edf29e8d17eb940f1d4
7SilviaT <eagles8485@gmail.com>
2022-04-22 21:40:52 -0500

    Design login and register pages

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+export default () => {
+  const homePage = `
+  <section id="welcomePage" >
+  <img src="./pics/welcome-pic.jpg" alt="Fondo bienvenida" class="welcomeBackgr">
+  <p class="title">ANIME GANG</p>
+  <button id="btnJoinUs"class="title"><a href="#/login">Únete</a></button>
+</section>
+  `;
+  const divElement = document.createElement('div');
+  divElement.innerHTML = homePage;
+  return divElement;
+};
--- src/components/login.js
+++ src/components/login.js
@@ -1,28 +1,26 @@
 export default () => {
   const loginPage = `
-    <section class="loginContainer">
-        <section class="subLoginContainer">
-          <div class="loginSubContainer"> 
-            <p class="loginTittle">Iniciar sesión</p>
-            <div class="inputContainer">
-            <input type="text" placeholder="Correo electrónico" id="inputEmail">
-            <p id="emptyInputEmail" class="error"></p>
-            <input type="password" placeholder="Contraseña" id="inputPass">
-            <p id="emptyInputPass" class="error"></p>
-            <button id="loginBtn" class="buttonnextPage"><a href="#/news"> Iniciar Sesión</a> </button>
-            </div>
-            <div class="withGoogle">
-            <p class="pGoogle">Iniciar sesión con google</p>
-            <img id="papeLogIn" style="display:none" src=./img/pape.png>
-            <img src="./img/googleLogo.png" class="logoGoogle" id="googleImgLogIn">
-            <p class="pGoogle">¿No tienes una cuenta?</p>
-            <button id="registerP" class="backpage"><a href="#/register">¿Eres nuevo por aquí? Registrate ahora</a></button>
-            </div>
-          </div>
-          </section>
-        </section>
-    `;
-
+<section class=" loginPage">
+  <img src="./pics/login-pic.jpg" alt="Fondo login" class="loginBackgr">
+  <div class="center-box" id="login-div">
+  <h2 >Iniciar sesión</h2>
+  <img src="./pics/userpicNekko.jpg" class="logoUser" id="userPic">
+  <div class="loginRegular">
+  <input type="email" id="login-email" class="text-field" name="login-email" placeholder="Correo electrónico" required/><br>
+  <input type="password" id="login-password" class="text-field" name="login-password" placeholder="Contraseña" required/><br>
+  <p id="emptyInputPass" class="error"></p>
+  <button id="loginBtn" class="buttonstyle"><a href="#/news"> Iniciar Sesión</a> </button> <br>
+  <a href="#/register">¿Eres nuevo por aquí? Registrate ahora</a>
+  </div>
+  <div class="loginGoogle">
+  <button class="pGoogle">Ingresa con tu cuenta Google
+  <img src="./pics/google-icon.png" class="logoGoogle" id="googleImgLogIn">
+  </button>
+  </div>
+</div>
+</div>
+</section>
+`;
   const divElement = document.createElement('div');
   divElement.innerHTML = loginPage;
   return divElement;
