53e68c4fc5ab3b4cf4b90b7ca5ff80642b21f9fb
Thalía Nohelia <thalia.cuadros.bautista@gmail.com>
2022-04-22 23:17:15 -0500

    Images Home

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,12 @@
 export default () => {
   const homePage = `
   <section id="welcomePage" >
+
   <div class="parent" >
+
     <img class="picture1" src="./pics/welcome-pic.jpg" alt="Fondo bienvenida" />
     <img class="picture2" src="./pics/flor-cerezo-pic.jpg" alt="Icono titulo" />
+    
   </div>
 
    <p class="title">ANIME GANG</p>
