4edf9660f962d1ac6766a61f5fd917fae7efb52e
Thalía Nohelia <thalia.cuadros.bautista@gmail.com>
2022-04-22 22:30:00 -0500

    Advance first view

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,13 @@
 export default () => {
   const homePage = `
   <section id="welcomePage" >
-  <img src="./pics/welcome-pic.jpg" alt="Fondo bienvenida" class="picture">
+  <div class="parent" >
+    <img class="picture1" src="./pics/welcome-pic.jpg" alt="Fondo bienvenida" />
+    <img class="picture2" src="./pics/flor-cerezo-pic.jpg" alt="Icono titulo" />
+  </div>
+
    <p class="title">ANIME GANG</p>
-   <button id="btnJoinUs"class="title"><a href="#/login">Únete</a></button>
+   <button id="btnJoinUs"class="btnJoinUs"><a href="#/login">Únete</a></button>
 </section>
   `;
   const divElement = document.createElement('div');
