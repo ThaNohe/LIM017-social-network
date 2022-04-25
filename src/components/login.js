ef048701105e4496836e6033706d3c1757d615af
7SilviaT <eagles8485@gmail.com>
2022-04-24 23:37:15 -0500

    css selectors/classes amended

--- src/components/login.js
+++ src/components/login.js
@@ -1,20 +1,20 @@
 export default () => {
   const loginPage = `
-<section class=" loginPage">
-  <img alt="Fondo login" class="loginBackgr">
+<section class=" login-page">
+  <img alt="Fondo login" class="login-page__background-pic">
   <div class="center-box" id="login-div">
   <h2 >Iniciar sesión</h2>
-  <img src="./pics/userpicNekko.jpg" class="logoUser" id="userPic">
-  <div class="loginRegular">
+  <img src="./pics/userpicNekko.jpg" class="logo-user" id="userPic">
+  <div class="login-page__form-newuser">
   <input type="email" id="login-email" class="text-field" name="login-email" placeholder="Correo electrónico" required/><br>
   <input type="password" id="login-password" class="text-field" name="login-password" placeholder="Contraseña" required/><br>
   <p id="emptyInputPass" class="error"></p>
-  <button id="loginBtn" class="buttonstyle"><a href="#/news"> Iniciar Sesión</a> </button> <br>
+  <button id="loginBtn" class="button-orange"><a href="#/news"> Iniciar Sesión</a> </button> <br>
   <a href="#/register">¿Eres nuevo por aquí? Registrate ahora</a>
   </div>
-  <div class="loginGoogle">
-  <button class="pGoogle">Ingresa con tu cuenta Google
-  <img src="./pics/google-icon.png" class="logoGoogle" id="googleImgLogIn">
+  <div class="login-page__form-google">
+  <button class="button-google">Ingresa con tu cuenta Google
+  <img src="./pics/google-icon.png" class="logo-google" id="googleImgLogIn">
   </button>
   </div>
 </div>
