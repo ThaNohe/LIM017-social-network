86472730e8a8650edc3fc56b90230db4dd994d7f
7SilviaT <eagles8485@gmail.com>
2022-04-28 22:21:20 -0500

    setting new firebase user and configutation

--- src/lib/authFunctions.js
+++ src/lib/authFunctions.js
@@ -20,23 +20,9 @@ export const registerWithEmail = (email, password) => {
     });
 };
 
-//auth del login usuario ya registrado,por revisar
+/* 1-auth del login usuario ya registrado
 
-export const signInWithEmailAndPassword =(auth, loginEmail, loginPassword) => {
-  signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-.then((userCredential) => {
-   const user = userCredential.user;
-  })
- .catch((error) => {
-   const errorCode = error.code;
-   const errorMessage = error.message;
-console.log(error);
- });
-};
-
-
-
-/* //auth del login usuario nuevo crea cuenta
+2-auth del login usuario nuevo crea cuenta
 document.getElementById("register-btn").addEventListener('click', function(){
 
     const registerEmail= document.getElementById("register-email").value;
--- src/lib/config.js
+++ src/lib/config.js
@@ -2,12 +2,12 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBZih3O-Z-TKcRE03kTfjZE6kDcR98vVTA',
-  authDomain: 'animegang-reloaded.firebaseapp.com',
-  projectId: 'animegang-reloaded',
-  storageBucket: 'animegang-reloaded.appspot.com',
-  messagingSenderId: '790386184835',
-  appId: '1:790386184835:web:984daae6e30b9eaddcc1b6',
+  apiKey: 'AIzaSyARl0JtBJwmmCcNBy_Q8V30YI1CiJdW8Iw',
+  authDomain: 'animegang-social.firebaseapp.com',
+  projectId: 'animegang-social',
+  storageBucket: 'animegang-social.appspot.com',
+  messagingSenderId: '531993327800',
+  appId: '1:531993327800:web:e136b5287c4499d3649c4d',
 };
 
 // Initialize Firebase
