2c22a6fd9d3a3feac2886baa9bb84d6a569efc3d
7SilviaT <eagles8485@gmail.com>
2022-05-02 10:26:24 -0500

    Fire auth register and email link ok

--- src/components/register.js
+++ src/components/register.js
@@ -1,42 +1,64 @@
-import { registerWithEmail, emailVerification } from '../lib/authFunctions.js';
+import { registerWithEmail, sendEmailVerificationFirebase } from '../lib/authFunctions.js';
 
 const registerDisplay = () => {
   const registerPage = `
   <section class=' register-page'>
   <div class= 'register-page-container'>
   <h2 class='title-login-register'>ANIME GANG</h2>
-  <img src='./pics/userpicNekko.jpg' class='logo-user' id='userPic'> <br>
-                <input type='text' id='inputName' placeholder='Usuario'> <br>
+  <img src='./pics/nekko-mascot.png' class='logo-user' id='userPic'> <br>
+  <p class='login-subtitle'> Registra tus datos </p>
+  <input type='text' id='inputName' class='text-field' placeholder='Nombre'>
+                <img src='./pics/user.png' class='logo-google' '> <br>
                 <p id='emptyInputName' class='error'></p>
-                <input type='email' id='inputEmail' placeholder='Correo electrónico' required> <br>
+                <input type='email' id='inputEmail' class='text-field' placeholder='Correo electrónico' required>
+                <img src='./pics/email.png' class='logo-google'> <br>
                 <p id='errorEmail' class='error'></p>
-                <input type='password' id='inputPassword'placeholder='Contraseña' required> <br>
+                <input type='password' id='inputPassword' class='text-field' placeholder='Contraseña' required> <br>
+                <img src='./pics/lock.png' class='logo-google'>
                 <p id='passError' class='error'></p>
                 <button id='btnRegister' class='button-login-orange'><a href='#/news'>Registrate ahora</a></button>
-            <div class='backLogin'>
             <p class='link-nextpage'><a href='#/login'>¿Ya tienes una cuenta? Iniciar Sesión</a></p>
+            <div class="line-google"><span> o </span></div>
             <div class='login-page__form-google'>
-  <button class='button-login-orange'>Ingresa con tu cuenta Google
+            <button class='button-login-orange'>Ingresa con tu cuenta
   <img src='./pics/google-icon.png' class='logo-google' id='googleImgLogIn'>
   </button>
   </div>
-            </div>
-        </div>
+      </div>
     </section>
 `;
 
   const divElement = document.createElement('div');
   divElement.innerHTML = registerPage;
 
-  const signUpForm = divElement.querySelector('#btnRegister').addEventListener('click', (e) => {
+  // const signUpForm =
+  divElement.querySelector('#btnRegister').addEventListener('click', (e) => {
     e.preventDefault();
     const userValue = document.getElementById('inputName').value;
     const registerEmailValue = document.getElementById('inputEmail').value;
     const registerPasswordValue = document.getElementById('inputPassword').value;
     console.log(userValue, registerEmailValue, registerPasswordValue);
-    registerWithEmail(registerEmailValue, registerPasswordValue);
-    emailVerification(registerEmailValue);
-    return signUpForm;
+    registerWithEmail(registerEmailValue, registerPasswordValue)
+      .then((userCredential) => {
+      // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        return user;
+      })
+      .then((user) => {
+        console.log(user);
+        sendEmailVerificationFirebase();
+        // eslint-disable-next-line no-alert
+        alert('Ya se envio tu correo de verificación');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log(errorCode);
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      // ..
+      });
+    // return signUpForm;
   });
   return divElement;
 };
--- src/lib/authFunctions.js
+++ src/lib/authFunctions.js
@@ -1,80 +1,23 @@
 // eslint-disable-next-line import/no-unresolved
-import { getAuth, createUserWithEmailAndPassword, sendSignInLinkToEmail } from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-auth.js';
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-auth.js';
 import { app } from './config.js';
 
 const auth = getAuth(app);
 
-export const registerWithEmail = (email, password) => {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      console.log(user);
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(error);
-      // ..
-    });
-};
+export const registerWithEmail = (email, password) => (
+  createUserWithEmailAndPassword(auth, email, password));
 
-// link de email de verificacion
-export const emailVerification = (email) => {
-  sendSignInLinkToEmail(auth.currentUser)
-    .then((result) => {
-      // Email verification sent!
-      console.log(result);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-/* 1-auth del login usuario ya registrado
+export const sendEmailVerificationFirebase = () => (
+  sendEmailVerification(auth.currentUser));
 
-2-auth del login usuario nuevo crea cuenta
-document.getElementById("register-btn").addEventListener('click', function(){
+// auth del login usuario ya registrado
+/* signInWithEmailAndPassword(auth, login-email, login-password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  }); */
 
-    const registerEmail= document.getElementById("register-email").value;
-    const registerPassword =document.getElementById("register-password").value;
-    createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
-   .then((userCredential) => {
-     const user = userCredential.user;
-     document.getElementById("result-box").style.display="inline";
-      document.getElementById("register-div").style.display="none";
-      document.getElementById("result").innerHTML="Bienvenido <br>"+registerEmail+" fue registrado";
-   }).catch((error) => {
-     const errorCode = error.code;
-     const errorMessage = error.message;
-     document.getElementById("result-box").style.display="inline";
-      document.getElementById("register-div").style.display="none";
-      document.getElementById("result").innerHTML="Sorry ! <br>"+errorMessage;
-   });
- });
-
- //auth del login usuario ya registrado
- document.getElementById("login-btn").addEventListener('click', function(){
-  const loginEmail= document.getElementById("login-email").value;
-  const loginPassword =document.getElementById("login-password").value;
-
-  signInWithEmailAndPassword(auth, loginEmail, loginPassword)
- .then((userCredential) => {
-   const user = userCredential.user;
-   document.getElementById("result-box").style.display="inline";
-    document.getElementById("login-div").style.display="none";
-    document.getElementById("result").innerHTML="Bienvenido <br>"+loginEmail;
- })
- .catch((error) => {
-   const errorCode = error.code;
-   const errorMessage = error.message;
-   document.getElementById("result-box").style.display="inline";
-    document.getElementById("login-div").style.display="none";
-    document.getElementById("result").innerHTML="Sorry ! <br>"+errorMessage;
-
- });
-});
-
-//auth con google
-
- */
+// auth con google
