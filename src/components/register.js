eed924436c51c6a36e4678791b23450b74ac2d77
7SilviaT <eagles8485@gmail.com>
2022-05-09 20:17:42 -0500

    Adding icons loginPage & test HTML for login/register/welcome pages

--- src/components/register.js
+++ src/components/register.js
@@ -1,3 +1,5 @@
+import { registerWithEmail, sendEmailVerificationFirebase } from '../lib/authFunctions.js';
+
 const registerDisplay = () => {
   const registerPage = `
   <section class=' register-page'>
@@ -10,19 +12,16 @@ const registerDisplay = () => {
         </div>
         <input type='text' id='inputName' class='text-field' placeholder='Nombre'>
         <p id='emptyInputName' class='error'></p>
-        
         <div class='icon-email-container'>
         <i class="fa-solid fa-at"></i>
         </div>
         <input type='email' id='inputEmail' class='text-field' placeholder='Correo electrónico' required>
-        <p id='errorEmail' class='error'> Ingresa un correo válido</p>
-        
+        <p id='errorEmail' class='error'></p>
         <div class='icon-password-container'>
         <i class="fa-solid fa-lock"></i>
         </div>
         <input type='password' id='inputPassword' class='text-field' placeholder='Contraseña' required>
         <p id='passError' class='error'></p>
-        
         <button id='btnRegister' class='button-login-orange'>Registrate ahora</button>
         <p class='link-nextpage'><a href='#/login'>¿Ya tienes una cuenta? Iniciar Sesión</a></p>
         <div class="line-google"><span> o </span></div>
@@ -37,16 +36,33 @@ const registerDisplay = () => {
 
   const divElement = document.createElement('div');
   divElement.innerHTML = registerPage;
-  return divElement;
-
-/*   divElement.querySelector('#btnRegister').addEventListener('click', (e) => {
-    e.preventDefault();
+  divElement.querySelector('#btnRegister').addEventListener('click', () => {
     const userValue = document.getElementById('inputName').value;
     const registerEmailValue = document.getElementById('inputEmail').value;
     const registerPasswordValue = document.getElementById('inputPassword').value;
     console.log(userValue, registerEmailValue, registerPasswordValue);
-
-  }); */
+    registerWithEmail(registerEmailValue, registerPasswordValue)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        return user;
+      })
+      .then((user) => {
+        console.log(user);
+        sendEmailVerificationFirebase();
+        // eslint-disable-next-line no-alert
+        alert('Ya se envio tu correo de verificación');
+        window.location.href = '#/login';
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log(errorCode);
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        // ..
+      });
+  });
+  return divElement;
 };
 export default registerDisplay;
 console.log(registerDisplay);
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,31 @@
 // importamos la funcion que vamos a testear
 import { changeView } from '../src/index.js';
 import home from '../src/components/home.js';
+import register from '../src/components/register.js';
+import login from '../src/components/login.js';
+import news from '../src/components/news.js';
 
 jest.mock('../src/lib/authFunctions.js');
 
 describe('changeView', () => {
-  it('changeView recibe un path home y devuelve el componente', () => {
+  it('changeView recibe un path HOME y devuelve el componente', () => {
     document.body.innerHTML = '<section id="container"></section>';
     const result = changeView('#/home');
     expect(result).toEqual(home());
+  });
+  it('changeView recibe un path REGISTER y devuelve el componente', () => {
+    document.body.innerHTML = '<section id="container"></section>';
+    const result = changeView('#/register');
+    expect(result).toEqual(register());
+  });
+  it('changeView recibe un path LOGIN y devuelve el componente', () => {
+    document.body.innerHTML = '<section id="container"></section>';
+    const result = changeView('#/login');
+    expect(result).toEqual(login());
+  });
+  it('changeView recibe un path NEWS y devuelve el componente', () => {
+    document.body.innerHTML = '<section id="container"></section>';
+    const result = changeView('#/news');
+    expect(result).toEqual(news());
   });
 });
