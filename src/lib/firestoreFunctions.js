8d4e4a23e73e49a292577898ac224a5ac82ae806
Thalía Nohelia <thalia.cuadros.bautista@gmail.com>
2022-05-11 18:56:05 -0500

    Funcion Delete

--- src/lib/firestoreFunctions.js
+++ src/lib/firestoreFunctions.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-unresolved */
 // eslint-disable-next-line import/no-unresolved
 import {
-  collection, addDoc, onSnapshot,
+  collection, addDoc, onSnapshot, deleteDoc, doc,
 } from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js';
 import { db } from './config.js';
 
 export const saveTask = (description, autor, createdAt) => addDoc(collection(db, 'tasks'), { description, autor, createdAt });
 export const onSnapshotFb = (callback) => onSnapshot(collection(db, 'tasks'), (callback));
+export const deletePost = (id) => deleteDoc(doc(db, 'tasks', id));
