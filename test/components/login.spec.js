adf327fc7c790fd283056699f3aa95b2f07577e2
7SilviaT <eagles8485@gmail.com>
2022-05-24 10:46:13 -0500

    Testing login component

--- test/components/login.spec.js
+++ test/components/login.spec.js
@@ -1,1 +1,21 @@
-jest.mock('../src/lib/firebaseFunctions.js');
+import { signInWithEmailAndPassword } from '../../src/lib/firebaseFunctions.js';
+
+import { loginWithEmail } from '../../src/lib/functionsController.js';
+
+jest.mock('../../src/lib/firebaseFunctions.js');
+
+// auth del login usuario ya registrado
+
+describe('parametros pasan por LOGIN function', () => {
+  // beforeEach(() => signInWithEmailAndPassword.mockClear());
+  // eslint-disable-next-line arrow-body-style
+  it('usuario se logea correctamente', () => {
+    return loginWithEmail('janedoe@gmail.com', 'passw')
+      .then((userCredential) => {
+        expect(userCredential.user.email).toBe('janedoe@gmail.com');
+        expect(signInWithEmailAndPassword.mock.calls[0])
+          .toEqual([{ _id: 'get-auth' }, 'janedoe@gmail.com', 'passw']);
+      // done();
+      });
+  });
+});
