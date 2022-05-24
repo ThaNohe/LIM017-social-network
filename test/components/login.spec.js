7905fa42b8ba7c6ccd04f59d6477f9a68152afdf
7SilviaT <eagles8485@gmail.com>
2022-05-24 12:46:59 -0500

    Testing part 2-login component

--- test/components/login.spec.js
+++ test/components/login.spec.js
@@ -1,12 +1,30 @@
 import { signInWithEmailAndPassword } from '../../src/lib/firebaseFunctions.js';
 
-import { loginWithEmail } from '../../src/lib/functionsController.js';
+import login from '../../src/components/login.js';
+
+// import { loginWithEmail } from '../../src/lib/functionsController.js';
 
 jest.mock('../../src/lib/firebaseFunctions.js');
 
+describe('Login function toma parametros ingresados', () => {
+  beforeEach(() => signInWithEmailAndPassword.mockClear());
+  it('funcion signIn de Firebase se ejecuta e inicia sesion', () => {
+    document.body.innerHTML = '<section id="container"></section>';
+    login();
+    document.getElementById('login-email').value = 'testing@aol.com';
+    document.getElementById('login-password').value = 'zzxxyy';
+    const btnLogin = document.getElementById('loginBtn');
+    btnLogin.dispatchEvent(new Event('click'));
+    console.log(signInWithEmailAndPassword.mock.calls[0]);
+    expect(document.getElementById('login-email').value).toEqual('testing@aol.com');
+    expect(document.getElementById('login-password').value).toEqual('zzxxyy');
+    expect(signInWithEmailAndPassword.mock.calls[0]).toEqual([{}, 'testing@aol.com', 'zzxxyy']);
+  });
+});
+
 // auth del login usuario ya registrado
 
-describe('parametros pasan por LOGIN function', () => {
+/* describe('parametros pasan por LOGIN function', () => {
   // beforeEach(() => signInWithEmailAndPassword.mockClear());
   // eslint-disable-next-line arrow-body-style
   it('usuario se logea correctamente', () => {
@@ -18,4 +36,4 @@ describe('parametros pasan por LOGIN function', () => {
       // done();
       });
   });
-});
+}); */
